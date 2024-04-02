a3f1f0dfac679b2b15d3841119e846231424e833
Kaceee17 <azkachandam@student.telkomuniversity.ac.id>
2024-04-03 01:16:11 +0700

    .

---
+++ resources/views/User/dashboard.blade.php
@@ -0,0 +1,22 @@
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+</head>
+<body>
+    <p>nyoba</p>
+    
+</body>
+</html> -->
+@extends('layouts.header')
+
+
+@section('content')
+
+<div></div>
+
+@endsection
+
+@extends('layouts.footer')
