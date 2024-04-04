6c5b6ac15fc5a5eb2271c97e8f10d8f986d44597
reihainifbo <116185290+reihainifbo@users.noreply.github.com>
2024-04-04 09:53:46 +0700

    Commit Rey Page Register

--- resources/views/Register/regist.blade.php
+++ resources/views/Register/regist.blade.php
@@ -31,26 +31,8 @@
     <div class="container">
         <div class="form-wrapper">
             <h1>Sign Up</h1>
-            <button type="button" class="active">Personal</button>
-            <button type="button">Business</button>
-            <div class="form-section active" id="personal-form">
-                <label for="name">Name:</label>
-                <input type="text" id="name" name="name" required>
-                <label for="email">Email:</label>
-                <input type="email" id="email" name="email" required>
-                <label for="password">Password:</label>
-                <input type="password" id="password" name="password" required>
-                <button type="submit">Sign Up</button>
-            </div>
-            <div class="form-section" id="business-form">
-                <label for="company">Company Name:</label>
-                <input type="text" id="company" name="company" required>
-                <label for="business-email">Email:</label>
-                <input type="email" id="business-email" name="business_email" required>
-                <label for="password">Password:</label>
-                <input type="password" id="password" name="password" required>
-                <button type="submit">Sign Up</button>
-            </div>
+            <button type="button" class="active">PSign Up as User</button>
+            <button type="button">Sign Up as Veterinarian</button>
         </div>
     </div>
 </body>
