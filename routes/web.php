f19a96fb1b6835d2c2cd5af6300b3792fb18cd2d
Kaceee17 <azkachandam@student.telkomuniversity.ac.id>
2024-04-05 17:32:01 +0700

    git commit -m "FCARE-30 <On Progress (85%)>"

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('dashboard');
 });
+Route::get('/header', function () {
+    return view('header');
+});
+Route::get('/footer', function () {
+    return view('footer');
+});
+
