2350518ebaaca502f8c7dc0e989b20172eac2e66
Kaceee17 <azkachandam@student.telkomuniversity.ac.id>
2024-04-05 13:25:58 +0700

    80%

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,8 +5,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Dashboard</title>
   <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
+  
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
+  @vite('resources/js/app.js')
 
 </head>
     <body>
