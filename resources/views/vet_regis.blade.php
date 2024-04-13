da1b251660abff21ba6e13cb7e79c4cc4f827da1
Putrinadia23 <putrinadia@student.telkomuniversity.ac.id>
2024-04-13 14:27:36 +0700

    veterenarian regis-putri

--- resources/views/vet_regis.blade.php
+++ resources/views/vet_regis.blade.php
@@ -3,18 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>login</title>
+    <title>FarmCare+ Registration</title>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    
     <style>
-       body, input, button, a {
+        body, input, button, a {
             font-family: 'Poppins', sans-serif;
-            
         }
         .registration-form {
             max-width: 400px;
             margin: 3rem auto;
-            font-weight: 500;
         }
         .header-area {
             text-align: center;
@@ -24,7 +23,11 @@
             max-width: 100%;
             height: auto;
         }
-    
+        .welcome-text {
+            font-size: 2rem;
+            font-weight: bold;
+            margin-bottom: 1rem;
+        }
         .file-input {
             border: 2px dashed #999;
             border-radius: .25rem;
@@ -32,15 +35,13 @@
             padding: 1rem;
             margin-bottom: 1rem;
         }
-        .login-button {
-            position: absolute;
-            bottom: 40px; 
-            left: 53%;
-            transform: translateX(-50%);
-            background-color: #8D7B68; 
+        .sign-up-button {
+            background-color: #795548; 
             border: none;
         }
-        
+        .sign-up-button:hover {
+            background-color: #5d4037; 
+        }
         .nav-tabs {
             border-bottom: 2px solid #000; 
         }
@@ -48,13 +49,12 @@
             border: none; 
             border-bottom: 2px solid #000; 
             color: black; 
-            
         }
         .nav-item {
             margin-right: 5px; 
         }
         .nav-link {
-            border: none; 
+            border: none;
             color: #aaa; 
         }
         .nav-link:hover {
@@ -90,6 +90,7 @@
     </style>
 </head>
 <body>
+
 <div class="container">
     <div class="card border-0">
         <div class="card-body">
@@ -97,7 +98,7 @@
             <div class="col-6" style="background-color: #FFF8F0 ; position: relative; height: 100vh;">
             <div class="header-area">
             <img src="images/logo.png" style="height: 500px; width: 450px; margin-top: -200px;" alt="Cows">
-    </div>
+            </div>
             <div class="welcome-text-container">
             <h2 class="welcome-text">Welcome to FarmCare+</h2>
     </div>
@@ -108,37 +109,72 @@
                             <div class="row">
                                 <div class="col-6">
                                     <div class="nav-item text-center">
-                                        <a class="nav-link active" href="login.html">Log in</a>
+                                        <a class="nav-link " href="login.html">Log in</a>
                                     </div>
                                 </div>
                                 <div class="col-6">
                                     <div class="nav-item text-center">
-                                        <a class="nav-link " href="register.html">Sign up</a>
+                                        <a class="nav-link active" href="register.html">Sign up</a>
                                     </div>
                                 </div>
                             </div>
                         </div>
 
                         <form>
-                                <div class="form-group">
-                                    <label class="form-label">Email</label>
-                                    <input type="email" class="form-control" placeholder="Email">
-                                </div>
-                                <div class="form-group ">
+                            <div class="form-group">
+                                <label class="form-label">Full Name</label>
+                                <input type="text" class="form-control" placeholder="Full name">
+                            </div>
+                            <div class="form-group">
+                                <label class="form-label">Email</label>
+
+                                <input type="email" class="form-control" placeholder="Email">
+                            </div>
+
+                            <div class="form-group">
+                                <label class="form-label">Specialist</label>
+
+                                <input type="text" class="form-control" placeholder="Specialist">
+                            </div>
+                            <div class="form-group">
+                                <label class="form-label">University</label>
+
+                                <input type="text" class="form-control" placeholder="University">
+                            </div>
+                            <div class="form-group">
+                                <label class="form-label">Graduate Year</label>
+
+                                <input type="text" class="form-control" placeholder="Graduate Year">
+                            </div>
+                            <div class="form-row">
+                                <div class="form-group col">
                                     <label class="form-label">Password</label>
+
                                     <input type="password" class="form-control" placeholder="Password">
                                 </div>
-                                <div class="form-group" style="margin-left: 20px">
-                                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                    <label class="form-check-label" for="exampleCheck1">Remember Me</label>
+                                <div class="form-group col">
+                                    <label class="form-label">Confirmation</label>
+
+                                    <input type="password" class="form-control" placeholder="Confirmation">
                                 </div>
-                            <button type="submit" class="btn login-button btn-block text-light">Log in</button>
+                            </div>
+                            <div class="file-input">
+                                <label class="form-label">Certification of Doctor</label>
+
+                                <input type="file" class="form-control-file">
+                                <small>Drag and drop here or browse</small>
+                            </div>
+                            <button type="submit" class="btn sign-up-button btn-block text-light">Sign up</button>
                         </form>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
+
+
 </div>
+
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('login_veterinarian');
+    return view('vet_regis');
 });
+
