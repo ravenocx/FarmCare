b5de7a7d212f7d61d598363b75e2f1dd1fc7edbb
ritsuhaaa <mharis830174@gmail.com>
2024-05-07 19:46:19 +0700

    FCARE-40 change admin login page

--- resources/views/pages/admin/login.blade.php
+++ resources/views/pages/admin/login.blade.php
@@ -12,10 +12,6 @@
 <body class="font-poppins">
     <section class="bg-shadeBrown">
     <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-white">
-            <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo">
-            Flowbite    
-        </a>
         <div class="w-full bg-white rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0 bg-gray-800 dark:border-gray-700">
             <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
                 <h1 class="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-xl dark:text-white">
