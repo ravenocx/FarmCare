b2607a3a9f04d715249c927be1afe487f14d62e5
ritsuhaaa <mharis830174@gmail.com>
2024-05-21 23:26:41 +0700

    FCARE-72 on progress update status online consult

--- resources/views/pages/veterinarian/consultation/index.blade.php
+++ resources/views/pages/veterinarian/consultation/index.blade.php
@@ -14,7 +14,8 @@
         <h2 class="font-semibold text-base mb-5">Schedules</h2>
         <a href="{{route('veterinarian.consultation.schedule')}}" class="font-semibold text-shadeBrown text-base"> View All ></a>
     </div>
-    <div class="flex justify-center">
+
+    <section class="flex justify-center pb-5 border-b-2 border-shadeCream mb-5">
         @foreach($serviceSchedules as $index => $serviceSchedule)
             <div class="{{ $index == 0 ? (count($serviceSchedules) === 1 ? '' : 'pr-6 border-r-2') : ($index == 1 ? 'px-6 border-r-2' : 'pl-6') }}  border-shadeCream">
                 <div class="flex text-xs">
@@ -46,7 +47,72 @@
                 </div>
             </div>
         @endforeach
-    </div>
+    </section>
+
+    <section class="pb-10 border-b-2 border-shadeCream mb-5">
+        <h2 class="font-semibold text-base mb-6">On Going Online Consultation</h2>
+
+        <div class="container mx-auto relative overflow-x-auto overflow-y-auto max-h-[321.32px] shadow-md sm:rounded-lg">
+            <table class="w-full text-base text-left rtl:text-right text-gray-500">
+                <thead class="text-base text-white font-medium bg-shadeBrown sticky top-0">
+                    <tr class="text-center">
+                        <th>
+                            No.
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Order ID
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Patient
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Patient Phone Number
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Invoice
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Status
+                        </th>
+                    </tr>
+                </thead>
+                <tbody class="text-center">
+                    @for ($i = 0; $i < 100; $i++)
+                    <tr class="odd:bg-white even:bg-gray-100 border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                        <td class="w-4 p-4">
+                            {{$i+1}}
+                        </td>
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            #123456
+                        </th>
+                        <td class="px-6 py-4">
+                            Reihaini Fikria Bunga
+                        </td>
+                        <td class="px-6 py-4">
+                            <a href="" class="underline">
+                                628219312013
+                            </a>
+                        </td>
+                        <td class="px-6 py-4">
+                            <a href="" class="underline">
+                                Invoice Image
+                            </a>
+                        </td>
+                        <td class="px-6 py-4 flex justify-center">
+                            <select id="status" class="text-gray-900 text-sm border-none focus:ring-gray-300 focus:rounded-lg block p-2">
+                                <option selected>On going</option>
+                                <option value="US">Completed</option>
+                                <option value="CA">Cancel</option>
+                            </select>
+                        </td>
+                    </tr>
+                    @endfor
+                </tbody>
+            </table>
+        </div>
+        
+    </section>
+
 
     @if (session('error'))
     <x-alert-notification type="error" message="{{session('error')}}"/>
