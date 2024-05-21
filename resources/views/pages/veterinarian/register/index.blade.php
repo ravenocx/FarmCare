2a58858760b0a3ba59cb4a7a203696e0e40b7734
ritsuhaaa <mharis830174@gmail.com>
2024-05-21 08:36:28 +0700

    change placeholder in phone number vet regiet

--- resources/views/pages/veterinarian/register/index.blade.php
+++ resources/views/pages/veterinarian/register/index.blade.php
@@ -42,7 +42,7 @@
                     <input id="graduateYear" type="number" name="graduateYear" placeholder="Enter your Graduate Year" class="w-[677px] mt-3 h-20 rounded-lg border border-shadeGray border-opacity-50 text-lg px-4" autofocus required >
                     
                     <label for="phone_number" class="font-medium text-2xl mt-6">Phone Number <span class="text-red-600">*</span></label>
-                    <input id="phone_number" type="text" name="phone_number" placeholder="Enter your Phone Number" class="w-[677px] mt-3 h-20 rounded-lg border border-shadeGray border-opacity-50 text-lg px-4" autofocus required >
+                    <input id="phone_number" type="text" name="phone_number" placeholder="628xxxxxxxxxxx" class="w-[677px] mt-3 h-20 rounded-lg border border-shadeGray border-opacity-50 text-lg px-4" autofocus required >
 
                     <label for="email" class="font-medium text-2xl mt-6">Email <span class="text-red-600">*</span></label>
                     <input id="email" type="email" name="email" placeholder="Enter your email" class="w-[677px] mt-3 h-20 rounded-lg border border-shadeGray border-opacity-50 text-lg px-4" autofocus required>
