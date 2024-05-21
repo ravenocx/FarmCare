eba21ca05a1df001334e20b242fe093c31220619
ritsuhaaa <mharis830174@gmail.com>
2024-05-21 16:46:46 +0700

    FCARE-136 read schedule in online consult done

--- resources/views/pages/veterinarian/consultation/index.blade.php
+++ resources/views/pages/veterinarian/consultation/index.blade.php
@@ -32,12 +32,17 @@
                 </div>
         
                 <div class="flex justify-center mt-5 space-x-3">
-                    <a href="">
-                        <button class="btn-base bg-shadeBrown font-bold text-xs text-white rounded py-2 px-5 hover:text-shadeBrown hover:bg-white hover:outline hover:outline-1">Edit</button>
-                    </a>
-                    <a href="">
-                        <button class="btn-base bg-[#DF0000] font-bold text-xs text-white rounded py-2 px-3">Cancel</button>
-                    </a>
+                    @if(Carbon\Carbon::parse($serviceSchedule->schedule_end)->isAfter(Carbon\Carbon::now()))
+                        <a href="{{route('veterinarian.consultation.schedule.edit' , ['id' => $serviceSchedule -> id])}}">
+                            <button class="btn-base bg-shadeBrown font-bold text-xs text-white rounded py-2 px-5 hover:text-shadeBrown hover:bg-white hover:outline hover:outline-1">Edit</button>
+                        </a>
+                        <a href="">
+                            <button class="btn-base bg-[#DF0000] font-bold text-xs text-white rounded py-2 px-3">Cancel</button>
+                        </a>
+                    @else
+                        <button class="btn-base bg-gray-300 text-xs font-bold text-white rounded py-2 px-5 cursor-not-allowed" disabled>Schedule Ended</button>
+                    
+                    @endif
                 </div>
             </div>
         @endforeach
