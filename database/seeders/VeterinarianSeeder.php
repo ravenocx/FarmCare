899d5139dc1fa2793611590dd3a4fd8ee1c0a601
Rafedo <rafedo@it.student.pens.ac.id>
2024-05-27 22:41:37 +0700

    seeder veterinarian

--- database/seeders/VeterinarianSeeder.php
+++ database/seeders/VeterinarianSeeder.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Faker\Factory as Faker;
 use Illuminate\Support\Facades\Hash;
-use Hash;
+
 
 
 class VeterinarianSeeder extends Seeder
