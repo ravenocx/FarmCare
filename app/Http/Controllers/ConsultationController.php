7996ab5e3bbd9df279fdaec8765aa4ff8347c5b7
ritsuhaaa <mharis830174@gmail.com>
2024-05-29 11:21:23 +0700

    FCARE-45 change controller logic

--- app/Http/Controllers/ConsultationController.php
+++ app/Http/Controllers/ConsultationController.php
@@ -26,109 +26,124 @@ class ConsultationController extends Controller
     }
     public function index()
     {
-        $now = $this->currentDateTime;
+        try{
+            $now = $this->currentDateTime;
         
-        $veterinariansRecommendation = Veterinarian::select('veterinarians.*')
-        ->selectRaw('COUNT(service_schedules.id) as service_schedules_count')
-        ->leftJoin('service_schedules', function ($join) use ($now) {
-            $join->on('veterinarians.id', '=', 'service_schedules.veterinarian_id')
-                ->where('service_schedules.is_reserved', false)
-                ->where('service_schedules.schedule_start', '<=', $now)
-                ->where('service_schedules.schedule_end', '>=', $now);
-        })
-        ->where('veterinarians.is_accepted', true)
-        ->groupBy('veterinarians.id')
-        ->orderBy('service_schedules_count', 'desc')
-        ->with(['serviceSchedules' => function ($query) use ($now) {
-            $query->where('is_reserved', false)
-                ->where('schedule_start', '<=', $now)
-                ->where('schedule_end', '>=', $now)
-                ->orderBy('schedule_start', 'asc')
-                ->limit(1)
-                ->get();
-        }])
-        ->orderBy('veterinarians.id', 'desc') // Order by veterinarian id descending
-        ->limit(2) // Limit the result to 2 veterinarians
-        ->get();
-        
-        $veterinariansBySpecialist = Veterinarian::select('veterinarians.*')
-        ->selectRaw('COUNT(service_schedules.id) as service_schedules_count')
-        ->leftJoin('service_schedules', function ($join) use ($now) {
-            $join->on('veterinarians.id', '=', 'service_schedules.veterinarian_id')
-                ->where('service_schedules.is_reserved', false)
-                ->where('service_schedules.schedule_start', '<=', $now)
-                ->where('service_schedules.schedule_end', '>=', $now);
-        })
-        ->where('veterinarians.is_accepted', true)
-        ->groupBy('veterinarians.id')
-        ->orderBy('veterinarians.specialist')
-        ->orderBy('service_schedules_count', 'desc') 
-        ->with(['serviceSchedules' => function ($query) use ($now) {
-            $query->where('is_reserved', false)
-                ->where('schedule_start', '<=', $now)
-                ->where('schedule_end', '>=', $now)
-                ->orderBy('schedule_start', 'asc')
-                ->limit(1)
-                ->get();
-        }])
-        ->get()
-        ->groupBy('specialist')
-        ->map(function ($group) {
-            return $group->take(3); // Limit to 3 veterinarians per specialist
-        });
+            $veterinariansRecommendation = Veterinarian::select('veterinarians.*')
+            ->selectRaw('COUNT(service_schedules.id) as service_schedules_count')
+            ->leftJoin('service_schedules', function ($join) use ($now) {
+                $join->on('veterinarians.id', '=', 'service_schedules.veterinarian_id')
+                    ->where('service_schedules.is_reserved', false)
+                    ->where('service_schedules.schedule_start', '<=', $now)
+                    ->where('service_schedules.schedule_end', '>=', $now);
+            })
+            ->where('veterinarians.is_accepted', true)
+            ->groupBy('veterinarians.id')
+            ->orderBy('service_schedules_count', 'desc')
+            ->with(['serviceSchedules' => function ($query) use ($now) {
+                $query->where('is_reserved', false)
+                    ->where('schedule_start', '<=', $now)
+                    ->where('schedule_end', '>=', $now)
+                    ->orderBy('schedule_start', 'asc')
+                    ->limit(1)
+                    ->get();
+            }])
+            ->orderBy('veterinarians.id', 'desc') // Order by veterinarian id descending
+            ->limit(2) // Limit the result to 2 veterinarians
+            ->get();
+            
+            $veterinariansBySpecialist = Veterinarian::select('veterinarians.*')
+            ->selectRaw('COUNT(service_schedules.id) as service_schedules_count')
+            ->leftJoin('service_schedules', function ($join) use ($now) {
+                $join->on('veterinarians.id', '=', 'service_schedules.veterinarian_id')
+                    ->where('service_schedules.is_reserved', false)
+                    ->where('service_schedules.schedule_start', '<=', $now)
+                    ->where('service_schedules.schedule_end', '>=', $now);
+            })
+            ->where('veterinarians.is_accepted', true)
+            ->groupBy('veterinarians.id')
+            ->orderBy('veterinarians.specialist')
+            ->orderBy('service_schedules_count', 'desc') 
+            ->with(['serviceSchedules' => function ($query) use ($now) {
+                $query->where('is_reserved', false)
+                    ->where('schedule_start', '<=', $now)
+                    ->where('schedule_end', '>=', $now)
+                    ->orderBy('schedule_start', 'asc')
+                    ->limit(1)
+                    ->get();
+            }])
+            ->get()
+            ->groupBy('specialist')
+            ->map(function ($group) {
+                return $group->take(3); // Limit to 3 veterinarians per specialist
+            });
 
 
-        return view('pages.user.consultation.index', compact('veterinariansRecommendation', 'veterinariansBySpecialist'))->with('breadcrumbs', $this->breadcrumbs);
+            return view('pages.user.consultation.index', compact('veterinariansRecommendation', 'veterinariansBySpecialist'))->with('breadcrumbs', $this->breadcrumbs);
+        }catch(ModelNotFoundException $e){
+            abort(404);
+        }
+        
     }
 
     public function getDoctorBySpecialist($specialist)
     {
-        $this->breadcrumbs = array_merge($this->breadcrumbs, array(['label' => $specialist, 'url' => route('user.consultation.specialist' , ['specialist' => $specialist])]));
+        try{
+            $this->breadcrumbs = array_merge($this->breadcrumbs, array(['label' => $specialist, 'url' => route('user.consultation.specialist' , ['specialist' => $specialist])]));
 
-        $now = $this->currentDateTime;
+            $now = $this->currentDateTime;
 
-        $veterinarians = Veterinarian::select('veterinarians.*')
-        ->selectRaw('COUNT(service_schedules.id) as service_schedules_count')
-        ->leftJoin('service_schedules', function ($join) use ($now) {
-            $join->on('veterinarians.id', '=', 'service_schedules.veterinarian_id')
-                ->where('service_schedules.is_reserved', false)
-                ->where('service_schedules.schedule_start', '<=', $now)
-                ->where('service_schedules.schedule_end', '>=', $now);
-        })
-        ->where('veterinarians.is_accepted', true)
-        ->where('veterinarians.specialist', $specialist)
-        ->groupBy('veterinarians.id')
-        ->orderBy('service_schedules_count', 'desc') 
-        ->with(['serviceSchedules' => function ($query) use ($now) {
-            $query->where('is_reserved', false)
-                ->where('schedule_start', '<=', $now)
-                ->where('schedule_end', '>=', $now)
-                ->orderBy('schedule_start', 'asc')
-                ->limit(1)
-                ->get();
-                
-        }])
-        ->paginate(15);
+            $veterinarians = Veterinarian::select('veterinarians.*')
+            ->selectRaw('COUNT(service_schedules.id) as service_schedules_count')
+            ->leftJoin('service_schedules', function ($join) use ($now) {
+                $join->on('veterinarians.id', '=', 'service_schedules.veterinarian_id')
+                    ->where('service_schedules.is_reserved', false)
+                    ->where('service_schedules.schedule_start', '<=', $now)
+                    ->where('service_schedules.schedule_end', '>=', $now);
+            })
+            ->where('veterinarians.is_accepted', true)
+            ->where('veterinarians.specialist', $specialist)
+            ->groupBy('veterinarians.id')
+            ->orderBy('service_schedules_count', 'desc') 
+            ->with(['serviceSchedules' => function ($query) use ($now) {
+                $query->where('is_reserved', false)
+                    ->where('schedule_start', '<=', $now)
+                    ->where('schedule_end', '>=', $now)
+                    ->orderBy('schedule_start', 'asc')
+                    ->limit(1)
+                    ->get();
+                    
+            }])
+            ->paginate(15);
 
-        return view('pages.user.consultation.specialist', compact('veterinarians', 'specialist'))->with('breadcrumbs', $this->breadcrumbs);
+            return view('pages.user.consultation.specialist', compact('veterinarians', 'specialist'))->with('breadcrumbs', $this->breadcrumbs);
+        }catch(ModelNotFoundException $e){
+            abort(404);
+        }
+        
     }
     private function prepareVeterinarianView($id, $viewName)
     {
-        $this->breadcrumbs = array_merge($this->breadcrumbs, array(['label' => 'Veterinarian Details', 'url' => route('user.consultation.veterinarian', ['id' => $id])]));
+        try{
+            $this->breadcrumbs = array_merge($this->breadcrumbs, array(['label' => 'Veterinarian Details', 'url' => route('user.consultation.veterinarian', ['id' => $id])]));
 
-        $now = $this->currentDateTime;
-        $veterinarians = Veterinarian::with(['serviceSchedules' => function ($query) use ($now) {
-            $query->where('is_reserved', false)
-                ->where('schedule_start', '<=', $now)
-                ->where('schedule_end', '>=', $now)
-                ->orderBy('schedule_start', 'asc')
-                ->limit(1)
-                ->get();
-        }])
-        ->where('veterinarians.is_accepted', true)
-        ->where('id',$id)
-        ->get();
-        return view($viewName, compact('veterinarians'))->with('breadcrumbs', $this->breadcrumbs);
+            $now = $this->currentDateTime;
+            $veterinarians = Veterinarian::with(['serviceSchedules' => function ($query) use ($now) {
+                $query->where('is_reserved', false)
+                    ->where('schedule_start', '<=', $now)
+                    ->where('schedule_end', '>=', $now)
+                    ->orderBy('schedule_start', 'asc')
+                    ->limit(1)
+                    ->get();
+            }])
+            ->where('veterinarians.is_accepted', true)
+            ->where('id',$id)
+            ->get();
+            return view($viewName, compact('veterinarians'))->with('breadcrumbs', $this->breadcrumbs);
+        }catch(ModelNotFoundException $e){
+            abort(404);
+        }
+        
     }
 
     public function getVeterinarianDetails($id)
