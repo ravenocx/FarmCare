fc218690620495a945706ad1c02c2bab8d1ec896
ritsuhaaa <mharis830174@gmail.com>
2024-06-09 05:21:58 +0700

    FCARE-166 add create consultation schedule test

---
+++ tests/Browser/ConsultationScheduleCreateTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+use Carbon\Carbon;
+
+class ConsultationScheduleCreateTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     */
+    public function testAddOnlineConsultationSchedule(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $currentDateTime = Carbon::now();
+
+            $browser->visit('/')
+                    ->assertSee('Login')
+                    ->press('Login')
+                    ->assertPathIs('/login')
+                    ->type('email', 'paris28@example.org')
+                    ->type('password', '123456')
+                    ->press('Sign In')
+                    ->clickLink('Online Consultation')
+                    ->assertPathIs('/veterinarian/consultation')
+                    ->press('Add Schedule')
+                    ->assertPathIs('/veterinarian/consultation/schedule/create')
+                    ->script([
+                        "document.querySelector('#datetime-start').value = '" . $currentDateTime->format('Y-m-d\TH:i') . "'",
+                        "document.querySelector('#datetime-end').value = '" . $currentDateTime->addHour()->format('Y-m-d\TH:i') . "'"
+                    ]);
+
+            $browser->press('Add Schedule')
+                    ->assertSee('Successfully add new online consultation schedule');
+        });
+    }
+}
