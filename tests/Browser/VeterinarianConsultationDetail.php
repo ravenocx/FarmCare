d1f53384c6a6369c0623a76b847255851f193bb2
ritsuhaaa <mharis830174@gmail.com>
2024-06-09 05:25:04 +0700

    FCARE-171 add read consultation order detail

---
+++ tests/Browser/VeterinarianConsultationDetail.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class VeterinarianConsultationDetail extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     */
+    public function testConsultationOrderDetail(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                    ->assertSee('Login')
+                    ->press('Login')
+                    ->assertPathIs('/login')
+                    ->type('email', 'paris28@example.org')
+                    ->type('password', '123456')
+                    ->press('Sign In')
+                    ->clickLink('Online Consultation')
+                    ->assertPathIs('/veterinarian/consultation')
+                    ->press('Order Detail')
+                    ->assertSee('Order History Detail');
+        });
+    }
+}
