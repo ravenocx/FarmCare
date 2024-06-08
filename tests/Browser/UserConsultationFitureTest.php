dcd4d474a489b4f6856357fc2d8511e11e70e185
ritsuhaaa <mharis830174@gmail.com>
2024-06-09 05:19:45 +0700

    FCARE-162 add user online consultation test

---
+++ tests/Browser/UserConsultationFitureTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class UserConsultationFitureTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     */
+    public function testGetOnlineConsultation(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                    ->assertSee('Login')
+                    ->press('Login')
+                    ->assertPathIs('/login')
+                    ->type('email', 'haris@gmail.com')
+                    ->type('password', '123123')
+                    ->press('Sign In')
+                    ->click('@service-summary')
+                    ->clickLink('Online Consultation')
+                    ->assertPathIs('/consultation')
+                    ->assertSee('Consult with a doctor at FarmCare+');
+        });
+    }
+}
