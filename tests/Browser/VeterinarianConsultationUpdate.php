f9b1dbd05364cab565532d3400105a0f308c2870
ritsuhaaa <mharis830174@gmail.com>
2024-06-09 05:24:35 +0700

    FCARE-170 add update consultation order status

---
+++ tests/Browser/VeterinarianConsultationUpdate.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class VeterinarianConsultationUpdate extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     */
+    public function testEditOnlineConsultationStatus(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                    ->assertSee('Login')
+                    ->press('Login')
+                    ->assertPathIs('/login')
+                    ->type('email', 'paris28@example.org')
+                    ->type('password', '123456')
+                    ->press('Sign In')
+                    ->clickLink('Online Consultation')
+                    ->assertPathIs('/veterinarian/consultation')
+                    ->select('status' , 'Complete')
+                    ->assertSee('Successfully edit online consultation status');
+        });
+    }
+}
