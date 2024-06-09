a9c3aa863c0933fd910eea903b0f5e2b857a858e
Kaceee17 <azkachandam@student.telkomuniversity.ac.id>
2024-06-10 00:52:55 +0700

    FCARE-183 add pause

--- tests/Browser/OrderHistoryTest.php
+++ tests/Browser/OrderHistoryTest.php
@@ -16,16 +16,25 @@ class OrderHistoryTest extends DuskTestCase
         $this->browse(function (Browser $browser) {
             $browser->visit('/')
                     ->assertSee('Login')
+                    ->pause(1000)
                     ->press('Login')
                     ->assertPathIs('/login')
+                    ->pause(1000)
                     ->type('email', 'paris28@example.org')
+                    ->pause(500)
                     ->type('password', '123456')
+                    ->pause(500)
                     ->press('Sign In')
+                    ->pause(1000)
                     ->assertSee('Order History')
                     ->clickLink('Order History')
+                    ->pause(1000)
                     ->assertPathIs('/veterinarian/order/history')
+                    ->pause(1000)
                     ->clickLink('Order Detail')
+                    ->pause(1000)
                     ->back();
         });
+        
     }
 }
