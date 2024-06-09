ab8a879a47c38578fa675b1b40fd21fa7775b1c7
farhanmuzam <farhanmuzam@gmail.com>
2024-06-10 01:30:10 +0700

    FCARE-207 Testing Fitur Delete Medication Delivery

---
+++ tests/Browser/DeleteMedicineTesting.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class DeleteMedicineTesting extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     */
+    public function testExample(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/login')
+                    ->type('email', 'banawa17@example.net')
+                    ->type('password', '123456')
+                    ->press('Sign In')
+                    ->assertPathIs('/veterinarian')
+                    ->clicklink('Online Consultation')
+                    ->visit('/veterinarian/consultation')
+                    ->press('Order Detail')
+                    ->visit('/veterinarian/consultation/order/detail/52')
+                    ->clicklink('View Medicine')
+                    ->click('[data-modal-toggle="deleteModal"]')
+                    ->press('Delete');
+        });
+    }
+}
