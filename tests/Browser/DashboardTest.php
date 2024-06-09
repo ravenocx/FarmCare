6ca41f46d9ac928d8370314cc6a7c083e87c5a13
Kaceee17 <azkachandam@student.telkomuniversity.ac.id>
2024-06-09 07:14:13 +0700

    AutomationTesting recommit

---
+++ tests/Browser/DashboardTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class DashboardTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     */
+    public function testExample(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                    ->assertSee('Login')
+                    ->press('Login')
+                    ->assertPathIs('/login')
+                    ->type('email', 'azka@gmail.com')
+                    ->type('password', '123456')
+                    ->press('Sign In')
+                    ->assertPathIs('/home');
+        });
+    }
+}
---
+++ tests/Browser/OrderHistoryTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class OrderHistoryTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     */
+    public function testExample(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                    ->assertSee('Login')
+                    ->press('Login')
+                    ->assertPathIs('/login')
+                    ->type('email', 'paris28@example.org')
+                    ->type('password', '123456')
+                    ->press('Sign In')
+                    ->assertSee('Order History')
+                    ->clickLink('Order History')
+                    ->assertPathIs('/veterinarian/order/history')
+                    ->clickLink('Order Detail')
+                    ->back();
+        });
+    }
+}
