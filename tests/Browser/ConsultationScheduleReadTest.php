92fccb74bb45b85f7600dbdc1616e78340a55975
ritsuhaaa <mharis830174@gmail.com>
2024-06-09 15:50:36 +0700

    FCARE-167 add login

--- tests/Browser/ConsultationScheduleReadTest.php
+++ tests/Browser/ConsultationScheduleReadTest.php
@@ -15,7 +15,15 @@ class ConsultationScheduleReadTest extends DuskTestCase
     {
         $this->browse(function (Browser $browser) {
 
-        $browser->clickLink('Online Consultation')
+        $browser->visit('/')
+                ->assertSee('Login')
+                ->press('Login')
+                ->assertPathIs('/login')
+                ->type('email', 'paris28@example.org')
+                ->type('password', '123456')
+                ->press('Sign In')
+                ->assertPathIs('/veterinarian')
+                ->clickLink('Online Consultation')
                 ->assertPathIs('/veterinarian/consultation')
                 ->click('@schedule-viewall')
                 ->assertPathIs('/veterinarian/consultation/schedule')
