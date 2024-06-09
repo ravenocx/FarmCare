66742c233d88fe1472ab63c5bf1a40bff0f42c50
reihainifbo <116185290+reihainifbo@users.noreply.github.com>
2024-06-09 19:46:08 +0700

    FCARE-180 <Read veterinarian edit profile test pass>

---
+++ tests/Browser/ReadAdminforApplicantTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class ReadAdminforApplicantTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     */
+    public function testExample(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/admin_login')
+                    ->type('email', 'admin@gmail.com')
+                    ->type('password', '123456')
+                    ->press('Sign in');
+        });
+    }
+}
